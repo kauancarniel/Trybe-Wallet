71143ba6560cfd10ab0a3f3721d0209bd08816ec
Kauan Carniel Peres <kauan-carniel@hotmail.com>
2023-06-25 13:09:31 -0300

    requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <div>
+        <header>
+          <table>
+            <thead>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </thead>
+          </table>
+        </header>
+      </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
